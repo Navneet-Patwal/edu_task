94fcc88bec13c9404d29b0a5b39850e97b5d60b0
Navneet <navneet.patwal31@gmail.com>
2025-04-21 19:30:20 +0530

    passowrd

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 const  connection = mysql.createConnection({ 
     host: 'localhost',
     user: 'navneet',
-    password: 'navneet',
+    password: 'navneet@789',
     database: 'school'
 });
 
